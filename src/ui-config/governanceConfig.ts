5b7dd53006faafe85eaf60b063d7c56092cd93e2
lovecoding <lovecodingdev@gmail.com>
2022-09-03 05:23:04 +0800

    build error. because rinkeby was removed.

--- src/ui-config/governanceConfig.ts
+++ src/ui-config/governanceConfig.ts
@@ -61,7 +61,7 @@ export const governanceConfig: GovernanceConfig = {
 };
 
 export const mntGovernanceConfig: MNTGovernanceConfig = {
-  chainId: ChainId.rinkeby,
+  chainId: ChainId.goerli,
   votingAssetName: 'MNT + stkMNT',
   averageNetworkBlockTime: 13.5,
   queryGovernanceDataUrl: 'https://api.thegraph.com/subgraphs/name/aave/governance-v2',
