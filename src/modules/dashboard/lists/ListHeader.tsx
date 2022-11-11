869890f5bd50896cdd98bb24c5b451cd4f4940be
johnlee <johnlee98713@gmail.com>
2022-11-12 00:58:33 +0800

    refactor: Assets table

--- src/modules/dashboard/lists/ListHeader.tsx
+++ src/modules/dashboard/lists/ListHeader.tsx
@@ -5,18 +5,12 @@ import { ListColumn } from '../../../components/lists/ListColumn';
 import { ListHeaderTitle } from '../../../components/lists/ListHeaderTitle';
 import { ListHeaderWrapper } from '../../../components/lists/ListHeaderWrapper';
 import { ListButtonsColumn } from './ListButtonsColumn';
-import { useMediaQuery, useTheme } from '@mui/material';
 
 interface ListHeaderProps {
   head: ReactNode[];
 }
 
 export const ListHeader = ({ head }: ListHeaderProps) => {
-  const theme = useTheme();
-  const downToLG = useMediaQuery(theme.breakpoints.down('lg'));
-  const downToMD = useMediaQuery(theme.breakpoints.down('md'));
-  const downToSM = useMediaQuery(theme.breakpoints.down('sm'));
-
   return (
     <ListHeaderWrapper>
       <ListColumn maxWidth={160} isRow>
@@ -26,10 +20,6 @@ export const ListHeader = ({ head }: ListHeaderProps) => {
       </ListColumn>
 
       {head.map((title, i) => {
-        if (downToLG && i == 1) return <></>;
-        if (downToMD && i == 5) return <></>;
-        if (downToSM && i == 0) return <></>;
-
         const values = title?.valueOf() as Record<string, string>;
         const isAction = values['key'] === 'Actions';
         return (
