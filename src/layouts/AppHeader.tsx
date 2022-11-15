ce473ebb6ed1287fafbf1653616b5203aa15a10d
johnlee <johnlee98713@gmail.com>
2022-11-16 03:02:32 +0800

    fix: update the size of logo

--- src/layouts/AppHeader.tsx
+++ src/layouts/AppHeader.tsx
@@ -123,7 +123,7 @@ export function AppHeader() {
           }}
           onClick={() => setMobileMenuOpen(false)}
         >
-          <img src={uiConfig.appLogo} alt="An SVG of an eye" width="48" />
+          <img src={uiConfig.appLogo} alt="An SVG of an eye" width="47" height="42" />
           {!xsm && <img src={uiConfig.monetariaText} alt="An SVG of an eye" />}
         </Box>
         <Box sx={{ mr: xsm ? 1 : 3 }}>
