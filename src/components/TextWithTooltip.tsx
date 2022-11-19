2ab085d43cdc66a71228283e1908760fc577235b
johnlee <johnlee98713@gmail.com>
2022-11-19 16:06:26 +0800

    fix: supplyassetTable

--- src/components/TextWithTooltip.tsx
+++ src/components/TextWithTooltip.tsx
@@ -23,7 +23,7 @@ export const TextWithTooltip = ({
   const [open, setOpen] = useState(false);
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'row', gap: '0.5em' }}>
+    <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'row', gap: '0.2em' }}>
       <Box>{text && <Typography {...rest}>{text}</Typography>}</Box>
       <Box>
         <ContentWithTooltip tooltipContent={<>{children}</>} open={open} setOpen={setOpen}>
