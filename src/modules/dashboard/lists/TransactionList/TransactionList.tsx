468aab4b65da7a13063b34176dbf6e71563a80c0
johnlee <johnlee98713@gmail.com>
2022-11-29 00:38:30 +0800

    feat: add table

--- src/modules/dashboard/lists/TransactionList/TransactionList.tsx
+++ src/modules/dashboard/lists/TransactionList/TransactionList.tsx
@@ -1,14 +1,17 @@
 import { Box, useMediaQuery, useTheme } from '@mui/material';
 import TransactionListHeader from './TransactionListHeader';
+import TransactionListItem from './TransactionListItem';
+import { ListHeader } from '../ListHeader';
 
 function TransactionList() {
   const theme = useTheme();
   const downToXSM = useMediaQuery(theme.breakpoints.down('xsm'));
+  const head = ['Type', 'Amount', 'Block', 'hash id', 'Date'];
 
   return (
     <Box sx={{ paddingTop: '10px' }}>
       <TransactionListHeader />
-      <Box>Ok</Box>
+      <Box>{!downToXSM && <TransactionListItem />}</Box>
     </Box>
   );
 }
---
+++ src/modules/dashboard/lists/TransactionList/TransactionListItem.tsx
@@ -0,0 +1,131 @@
+import { useState } from 'react';
+import { Button, Box, useMediaQuery, useTheme } from '@mui/material';
+import { Table } from 'rsuite';
+import { TokenIcon } from 'src/components/primitives/TokenIcon';
+
+const { Column, HeaderCell, Cell } = Table;
+const data = [
+  {
+    asset: 'ETH',
+    symbol: 'eth',
+    network: 'Ethereum',
+    type: 'ETH borrow',
+    amount: 237.5,
+    block: 'YES',
+    hash_id: '2342342342342',
+    date: '16/12/2022',
+  },
+  {
+    asset: 'BTC',
+    symbol: 'btc',
+    network: 'BTC',
+    type: 'ETH borrow',
+    amount: 237.5,
+    block: 'YES',
+    hash_id: '2342342342342',
+    date: '16/12/2022',
+  },
+];
+
+export const TransactionListItem = () => {
+  const [sortColumn, setSortColumn] = useState();
+  const [sortType, setSortType] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const getData = () => {
+    if (sortColumn && sortType) {
+      return data.sort((a, b) => {
+        let x = a[sortColumn];
+        let y = b[sortColumn];
+        if (typeof x === 'string') {
+          x = x.charCodeAt();
+        }
+        if (typeof y === 'string') {
+          y = y.charCodeAt();
+        }
+        if (sortType === 'asc') {
+          return x - y;
+        } else {
+          return y - x;
+        }
+      });
+    }
+    return data;
+  };
+
+  const handleSortColumn = (sortColumn, sortType) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setSortColumn(sortColumn);
+      setSortType(sortType);
+    }, 500);
+  };
+
+  return (
+    <Table
+      height={420}
+      data={getData()}
+      sortColumn={sortColumn}
+      sortType={sortType}
+      onSortColumn={handleSortColumn}
+      loading={loading}
+      cellBordered={false}
+      rowHeight={60}
+    >
+      <Column width={150} align="center" fixed sortable verticalAlign="middle">
+        <HeaderCell>Asset</HeaderCell>
+        <Cell>
+          {(rowData) => (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: 2,
+                justifyContent: 'left',
+                alignItems: 'center',
+              }}
+            >
+              <TokenIcon
+                symbol={rowData.symbol}
+                sx={{ fontSize: `24px`, ml: -1 }}
+                key={rowData.symbol}
+              />
+              <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.05em' }}>
+                <Box sx={{ fontSize: '14px', fontWeight: 400 }}>{rowData.asset}</Box>
+                <Box sx={{ fontSize: '12px', fontWeight: 400, color: '#84919A' }}>
+                  {rowData.network}
+                </Box>
+              </Box>
+            </Box>
+          )}
+        </Cell>
+      </Column>
+
+      <Column width={150} align="left" sortable verticalAlign="middle">
+        <HeaderCell>Type</HeaderCell>
+        <Cell dataKey="type" />
+      </Column>
+
+      <Column width={150} align="left" sortable verticalAlign="middle">
+        <HeaderCell>Amount</HeaderCell>
+        <Cell dataKey="amount" />
+      </Column>
+
+      <Column width={100} align="left" sortable verticalAlign="middle">
+        <HeaderCell>Block</HeaderCell>
+        <Cell dataKey="block" />
+      </Column>
+
+      <Column width={260} align="right" sortable verticalAlign="middle">
+        <HeaderCell>Hash Id</HeaderCell>
+        <Cell dataKey="hash_id" />
+      </Column>
+      <Column width={130} align="left" sortable fixed="right" verticalAlign="middle">
+        <HeaderCell>Date</HeaderCell>
+        <Cell dataKey="date" />
+      </Column>
+    </Table>
+  );
+};
+export default TransactionListItem;
