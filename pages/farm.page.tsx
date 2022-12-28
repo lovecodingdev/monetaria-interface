eaed5bbc1571c46b001fa5cdf710e973d1105fa4
johnlee713 <johnlee98713@gmail.com>
2022-12-28 17:57:56 +0800

    feat: add contents of farm

--- pages/farm.page.tsx
+++ pages/farm.page.tsx
@@ -1,9 +1,14 @@
-import { Box, Container, Paper, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Container, Paper, Typography, useMediaQuery, useTheme, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { MainLayout } from '../src/layouts/MainLayout';
 import borderGradient from 'src/layouts/borderGradient';
-import { InputNumber } from 'rsuite';
+import { InputNumber, Input, InputGroup } from 'rsuite';
+import SearchIcon from '@rsuite/icons/Search';
 import 'rsuite/dist/rsuite.min.css';
+
+import BISWAP from '/public/icons/swap/bsw.svg';
+import PANCAKESWAP from '/public/icons/swap/pancakeswap.svg';
+import MDEX from '/public/icons/swap/mdex.svg';
 
 export default function Farm() {
   const { breakpoints } = useTheme();
@@ -29,7 +34,100 @@ export default function Farm() {
           color: '#F1F1F3',
           ...borderGradient,
         }}
-      ></Paper>
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '24px' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Box>
+              <Typography sx={{ fontWeight: 500, fontSize: '20px', color: '#080F26' }}>
+                Active Pools
+              </Typography>
+            </Box>
+            <Box>
+              <InputGroup>
+                <Input placeholder="Search token" />
+                <InputGroup.Button>
+                  <SearchIcon />
+                </InputGroup.Button>
+              </InputGroup>
+            </Box>
+          </Box>
+          <Box>
+            <label
+              style={{
+                display: 'block',
+                color: '#252C32',
+                fontWeight: 400,
+                fontSize: '14px',
+                paddingBottom: '5px',
+              }}
+            >
+              DEX:
+            </label>
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4px', flexWrap: 'wrap' }}>
+              <Box>
+                <Button
+                  sx={{
+                    backgroundColor: '#074592',
+                    color: '#FFF',
+                    fontWeight: 700,
+                    fontSize: '12px',
+                  }}
+                  variant="contained"
+                >
+                  All
+                </Button>
+              </Box>
+              <Box>
+                <Button
+                  sx={{
+                    backgroundColor: '#F6F8F9',
+                    color: '#000',
+                    fontWeight: 700,
+                    fontSize: '12px',
+                    width: '144px',
+                  }}
+                  startIcon={<BISWAP />}
+                  variant="outlined"
+                >
+                  Biswap
+                </Button>
+              </Box>
+              <Box>
+                {' '}
+                <Button
+                  sx={{
+                    backgroundColor: '#F6F8F9',
+                    color: '#000',
+                    fontWeight: 700,
+                    fontSize: '12px',
+                    width: '144px',
+                  }}
+                  startIcon={<PANCAKESWAP />}
+                  variant="outlined"
+                >
+                  PancakeSwap
+                </Button>
+              </Box>
+              <Box>
+                {' '}
+                <Button
+                  sx={{
+                    backgroundColor: '#F6F8F9',
+                    color: '#000',
+                    fontWeight: 700,
+                    fontSize: '12px',
+                    width: '144px',
+                  }}
+                  startIcon={<MDEX />}
+                  variant="outlined"
+                >
+                  MDEX
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Paper>
     </Container>
   );
 }
