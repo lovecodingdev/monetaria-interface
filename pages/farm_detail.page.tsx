d090d74aa1456a97cbd86c374665345311f386bd
johnlee713 <johnlee98713@gmail.com>
2022-12-29 23:01:37 +0800

    feat: add contents

--- pages/farm_detail.page.tsx
+++ pages/farm_detail.page.tsx
@@ -11,7 +11,7 @@ import {
 import { useEffect, useState } from 'react';
 import { MainLayout } from '../src/layouts/MainLayout';
 import borderGradient from 'src/layouts/borderGradient';
-import { SelectPicker, InputNumber, ButtonToolbar, ButtonGroup } from 'rsuite';
+import { InputGroup, Input } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
 import { BaseNetworkConfig } from 'src/ui-config/networksConfig';
 import {
@@ -21,6 +21,7 @@ import {
   marketsData,
   networkConfigs,
 } from 'src/utils/marketsAndNetworksConfig';
+import Slider from '@mui/material/Slider';
 import { TokenIcon } from 'src/components/primitives/TokenIcon';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import MntShiba from '/public/icons/tokens/mnt-shiba.svg';
@@ -61,11 +62,33 @@ availableMarkets.map((marketId: CustomMarket) => {
   });
 });
 
+const marks = [
+  {
+    value: 1,
+    label: '1.00x',
+  },
+  {
+    value: 1.5,
+    label: '1.50x',
+  },
+  {
+    value: 2,
+    label: '2.00x',
+  },
+];
+
 export default function FarmDetail() {
   const { breakpoints } = useTheme();
   const xsm = useMediaQuery(breakpoints.up('xsm'));
   const downToXSM = useMediaQuery(breakpoints.down('xsm'));
   const [amtOfMnt, setAmtOfMnt] = useState(0);
+  const [amtOfCoin, setAmtOfCoin] = useState(0);
+  const [times, setTimes] = useState(1.5);
+
+  const valuetext = (value: number) => {
+    setTimes(value);
+    return `${value} Week(s)`;
+  };
 
   return (
     <Container
@@ -229,6 +252,192 @@ export default function FarmDetail() {
                 >
                   100%
                 </Button>
+              </Box>
+              <Box>
+                <label
+                  style={{
+                    display: 'block',
+                    color: '#252C32',
+                    fontWeight: 400,
+                    fontSize: '14px',
+                    paddingBottom: '5px',
+                  }}
+                >
+                  COIN
+                </label>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    backgroundColor: '#F6F8F9',
+                    border: '1px solid #EEF0F2',
+                    height: '48px',
+                    alignItems: 'center',
+                    padding: '0 17px',
+                    borderRadius: '8px',
+                  }}
+                >
+                  <Box sx={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
+                    <TokenIcon symbol={'bnb'} sx={{ fontSize: '24px', mr: 1 }} />
+                    <input
+                      type="number"
+                      className="stake-input"
+                      placeholder="0.00"
+                      step={0.01}
+                      value={amtOfCoin}
+                      onChange={(e) => setAmtOfCoin(e.target.value)}
+                      style={{
+                        outline: 'none',
+                        border: 'none',
+                        backgroundColor: 'transparent',
+                        fontWeight: 500,
+                        fontSize: '18px',
+                        color: '#D5DADD',
+                        width: downToXSM ? '120px' : '100%',
+                      }}
+                    />
+                  </Box>
+                  <Box sx={{ display: 'flex', flexDirection: 'row', gap: '4px' }}>
+                    <Typography sx={{ color: '#9AA6AC', fontWeight: 400, fontSize: '12px' }}>
+                      Balance: <span style={{ fontWeight: 600 }}>179 COIN</span>
+                    </Typography>
+                    <Typography
+                      sx={{
+                        color: '#9AA6AC',
+                        fontWeight: 400,
+                        fontSize: '12px',
+                        textDecoration: 'underline',
+                      }}
+                    >
+                      Buy
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Box sx={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
+                <Button
+                  sx={{
+                    flex: 1,
+                    backgroundColor: '#EEF0F2',
+                    color: '#000000',
+                    borderRadius: '8px',
+                    height: '33px',
+                    fontSize: '14px',
+                    fontWeight: 600,
+                  }}
+                >
+                  25%
+                </Button>
+                <Button
+                  sx={{
+                    flex: 1,
+                    backgroundColor: '#EEF0F2',
+                    color: '#000000',
+                    borderRadius: '8px',
+                    height: '33px',
+                    fontSize: '14px',
+                    fontWeight: 600,
+                  }}
+                >
+                  50%
+                </Button>
+                <Button
+                  sx={{
+                    flex: 1,
+                    backgroundColor: '#EEF0F2',
+                    color: '#000000',
+                    borderRadius: '8px',
+                    height: '33px',
+                    fontSize: '14px',
+                    fontWeight: 600,
+                  }}
+                >
+                  75%
+                </Button>
+                <Button
+                  sx={{
+                    flex: 1,
+                    backgroundColor: '#EEF0F2',
+                    color: '#000000',
+                    borderRadius: '8px',
+                    height: '33px',
+                    fontSize: '14px',
+                    fontWeight: 600,
+                  }}
+                >
+                  100%
+                </Button>
+              </Box>
+              <Box>
+                <label
+                  style={{
+                    display: 'block',
+                    color: '#252C32',
+                    fontWeight: 400,
+                    fontSize: '14px',
+                    paddingBottom: '5px',
+                  }}
+                >
+                  Leverage
+                </label>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '12px',
+                    flexWrap: 'wrap',
+                  }}
+                >
+                  <Box sx={{ flex: 3, padding: '0 10px' }}>
+                    {' '}
+                    <Slider
+                      aria-label="Custom marks"
+                      defaultValue={1.5}
+                      step={0.5}
+                      valueLabelDisplay="auto"
+                      getAriaValueText={valuetext}
+                      min={1}
+                      max={2}
+                      marks={marks}
+                      sx={{
+                        '& .MuiSlider-thumb': {
+                          backgroundColor: '#F6F8F9',
+                          border: '1px solid #B0BABF',
+                          opacity: 1,
+                          '&:focus, &:hover, &.Mui-active': {
+                            boxShadow:
+                              '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+                            // Reset on touch devices, it doesn't add specificity
+                          },
+                        },
+                        '& .MuiSlider-track': {
+                          border: 'none',
+                          backgroundColor: '#074592',
+                        },
+                        '& .MuiSlider-rail': {
+                          opacity: 1,
+                          backgroundColor: '#DDE2E4',
+                        },
+                        '& .MuiSlider-mark': {
+                          backgroundColor: '#bfbfbf',
+                          height: '2px',
+                          '&.MuiSlider-markActive': {
+                            opacity: 1,
+                            backgroundColor: '#F6F8F9',
+                          },
+                        },
+                      }}
+                    />
+                  </Box>
+                  <Box sx={{ flex: 1 }}>
+                    {' '}
+                    <InputGroup inside>
+                      <Input value={times} />
+                      <InputGroup.Addon>X</InputGroup.Addon>
+                    </InputGroup>
+                  </Box>
+                </Box>
               </Box>
             </Box>
           </Paper>
