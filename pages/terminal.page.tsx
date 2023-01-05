26531f1a4af6c14c6c343cea5f1389c8842f17c0
johnlee713 <johnlee98713@gmail.com>
2023-01-05 23:37:34 +0800

    feat: create terminal page

---
+++ pages/terminal.page.tsx
@@ -0,0 +1,84 @@
+import { Trans } from '@lingui/macro';
+import { Box, Paper, Typography, useMediaQuery, useTheme, Button, Tab, Tabs } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { usePermissions } from 'src/hooks/usePermissions';
+import { ConnectWalletPaper } from '../src/components/ConnectWalletPaper';
+import { ContentContainer } from '../src/components/ContentContainer';
+import { MainLayout } from '../src/layouts/MainLayout';
+import { useWeb3Context } from '../src/libs/hooks/useWeb3Context';
+import borderGradient from 'src/layouts/borderGradient';
+
+export default function Votes() {
+  const { currentAccount, loading: web3Loading } = useWeb3Context();
+  const { isPermissionsLoading } = usePermissions();
+  const { breakpoints } = useTheme();
+  const xsm = useMediaQuery(breakpoints.up('xsm'));
+  const downToXSM = useMediaQuery(breakpoints.down('xsm'));
+
+  return (
+    <>
+      <ContentContainer>
+        {currentAccount && !isPermissionsLoading ? (
+          <Box sx={{ fontFamily: 'Gilroy, Arial !important', fontStyle: 'normal' }}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: { xs: 'column', sm: 'row', gap: '24px' },
+                flexWrap: 'wrap',
+              }}
+            >
+              <Box sx={{ flex: 2 }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+                  <Paper
+                    sx={{
+                      bgcolor: 'background.header',
+                      padding: '24px',
+                      mt: { xs: '8px', md: '12px' },
+                      color: '#F1F1F3',
+                      ...borderGradient,
+                    }}
+                  ></Paper>
+                  <Paper
+                    sx={{
+                      bgcolor: 'background.header',
+                      padding: '24px',
+                      mt: { xs: '8px', md: '12px' },
+                      color: '#F1F1F3',
+                      ...borderGradient,
+                    }}
+                  ></Paper>
+                  <Paper
+                    sx={{
+                      bgcolor: 'background.header',
+                      padding: '24px',
+                      mt: { xs: '8px', md: '12px' },
+                      color: '#F1F1F3',
+                      ...borderGradient,
+                    }}
+                  ></Paper>
+                </Box>
+              </Box>
+              <Box sx={{ flex: 1 }}>
+                <Paper
+                  sx={{
+                    bgcolor: 'background.header',
+                    padding: '24px',
+                    mt: { xs: '8px', md: '12px' },
+                    color: '#F1F1F3',
+                    ...borderGradient,
+                  }}
+                ></Paper>
+              </Box>
+            </Box>
+          </Box>
+        ) : (
+          <ConnectWalletPaper loading={web3Loading} />
+        )}
+      </ContentContainer>
+    </>
+  );
+}
+
+Votes.getLayout = function getLayout(page: React.ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
